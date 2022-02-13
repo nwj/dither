f1ebf3fb3667fba50d74e09aa8f8eae311ba6738
Nick Willett-Jeffries <nwillettjeffries@gmail.com>
2022-02-13 07:56:38 -0500

    Add mode argument to allow for different dithering algorithms

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 use anyhow::Result;
-use clap::Parser;
+use clap::{ArgEnum, Parser};
 use image::io::Reader as ImageReader;
-use image::GrayImage;
-use image::Luma;
+use image::{GrayImage, Luma};
 use std::path::PathBuf;
 
 #[derive(Parser)]
@@ -11,13 +10,24 @@ struct Args {
     input_path: PathBuf,
     #[clap(parse(from_str))]
     output_path: PathBuf,
+    #[clap(short,long, arg_enum, default_value_t = Mode::Naive1d)]
+    mode: Mode,
+}
+
+#[derive(Copy, Clone, PartialEq, Eq, ArgEnum)]
+enum Mode {
+    Naive1d,
+    Naive2d,
 }
 
 fn main() -> Result<()> {
     let args = Args::parse();
     let mut img = ImageReader::open(args.input_path)?.decode()?.to_luma8();
 
-    naive_1d_dither(&mut img);
+    match args.mode {
+        Mode::Naive1d => naive_1d_dither(&mut img),
+        Mode::Naive2d => {}
+    }
 
     img.save(args.output_path)?;
     Ok(())
